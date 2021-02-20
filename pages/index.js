fe9a39e3be962a78fb2ce865100b8c1bde43aef3
Christophe Ghisi <christophe.ghisi@gmail.com>
2021-02-20 11:39:20 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,30 @@
-import { Client } from "../prismic";
-import SliceZone from "next-slicezone";
-import { useGetStaticProps, useGetStaticPaths } from "next-slicezone/hooks";
+// import { Client } from "../prismic";
+// import SliceZone from "next-slicezone";
+// import { useGetStaticProps, useGetStaticPaths } from "next-slicezone/hooks";
 
-import resolver from "../sm-resolver";
+// import resolver from "../sm-resolver";
 
-import Layout from "./../components/Layout";
+// import Layout from "./../components/Layout";
 
-const HomePage = (props) => {
-  return (
-    <Layout menu={props.menu}>
-      <SliceZone {...props} resolver={resolver} />
-    </Layout>
-  );
-};
+// const HomePage = (props) => {
+//   return (
+//     <Layout menu={props.menu}>
+//       <SliceZone {...props} resolver={resolver} />
+//     </Layout>
+//   );
+// };
 
-// Fetch content from prismic
-export const getStaticProps = useGetStaticProps({
-  client: Client(),
-  uid: "homepage",
-});
+// // Fetch content from prismic
+// export const getStaticProps = useGetStaticProps({
+//   client: Client(),
+//   uid: "homepage",
+// });
 
-export const getStaticPaths = useGetStaticPaths({
-  client: Client(),
-  type: "page",
-  fallback: true, // process.env.NODE_ENV === 'development',
-  //formatPath: ({ uid }) => ({ params: { uid } }),
-});
+// export const getStaticPaths = useGetStaticPaths({
+//   client: Client(),
+//   type: "page",
+//   fallback: true, // process.env.NODE_ENV === 'development',
+//   //formatPath: ({ uid }) => ({ params: { uid } }),
+// });
 
-export default HomePage;
+// export default HomePage;
