2b52d905fea532915410b7519cc2a0a57c115f49
Christophe Ghisi <christophe.ghisi@gmail.com>
2021-02-20 13:13:09 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -24,7 +24,7 @@ export const getStaticPaths = useGetStaticPaths({
   client: Client(),
   type: "page",
   fallback: true, // process.env.NODE_ENV === 'development',
-  formatPath: () => ({ params: { uid: "home" } }),
+  formatPath: () => ({ params: { uid: "homepage" } }),
 });
 
 export default Page;
