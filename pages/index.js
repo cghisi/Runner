4c1abc04c5f2da73cee20b879fbe26d5f5c50367
Christophe Ghisi <christophe.ghisi@gmail.com>
2021-02-20 11:42:16 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,19 @@
-// import { Client } from "../prismic";
-// import SliceZone from "next-slicezone";
-// import { useGetStaticProps, useGetStaticPaths } from "next-slicezone/hooks";
+import { Client } from "../prismic";
+import SliceZone from "next-slicezone";
+import { useGetStaticProps, useGetStaticPaths } from "next-slicezone/hooks";
 
-// import resolver from "../sm-resolver";
+import resolver from "../sm-resolver";
 
-// import Layout from "./../components/Layout";
+import Layout from "./../components/Layout";
 
-// const HomePage = (props) => {
-//   return (
-//     <Layout menu={props.menu}>
-//       <SliceZone {...props} resolver={resolver} />
-//     </Layout>
-//   );
-// };
+const HomePage = (props) => {
+  return (
+    <div>test</div>
+    // <Layout menu={props.menu}>
+    //   <SliceZone {...props} resolver={resolver} />
+    // </Layout>
+  );
+};
 
 // // Fetch content from prismic
 // export const getStaticProps = useGetStaticProps({
@@ -27,4 +28,4 @@
 //   //formatPath: ({ uid }) => ({ params: { uid } }),
 // });
 
-// export default HomePage;
+export default HomePage;
